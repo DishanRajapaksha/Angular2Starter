81b90696e5424a773918cdff7194cca8e8babcf7
dsrajapaksha <dsrajapaksha@outlook.com>
2017-02-05 17:57:26 +0530

    11 Apply CSS Classes Conditionally with Angular 2’s ngClass

--- src/app/simpe-form/simple-form.component.ts
+++ src/app/simpe-form/simple-form.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
       <!-- #myInput is a Reference to the input element -->
       <!-- ngModel's () represents and event, [] represents inputs, Two way binding -->
-      <input #myInput type='text' [(ngModel)]="message">
+      <input #myInput type='text' [(ngModel)]="message" [ngClass]="{mousedown:isMouseDown}"
+      (mousedown)="isMouseDown = true" (mouseenter)="isMouseDown = true" (mouseup)="isMouseDown = false" (mouseleave)="isMouseDown = false">
     
       <!-- Creates a (click) event and assigns a method created in the class -->
       <!-- Button click event is taken from the $event, event can be click, mouseover or anything -->
@@ -15,7 +16,25 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
     
     </div>
   `,
-  styles: []
+  styles: [`
+    :host{
+      display:flex;
+      flex-direction:column;
+    }
+
+    input:focus{
+      font-weight:bold;
+      outline:none;
+    }
+
+    button{
+      border:none;
+    }
+
+    .mousedown{
+      border:2px solid green;
+    }
+  `]
 })
 export class SimpleFormComponent implements OnInit {
 
@@ -24,6 +43,9 @@ export class SimpleFormComponent implements OnInit {
   @Input() message;
 
   @Output() update = new EventEmitter();
+
+  isMouseDown;
+
 
   ngOnInit() {
   }
