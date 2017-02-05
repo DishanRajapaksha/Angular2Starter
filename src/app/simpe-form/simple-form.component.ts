e15c00effb31a4fcf2a31b692f9a594a13966259
Dishan Rajapaksha <Dishan Rajapaksha>
2017-02-05 16:35:35 +0530

    3 Manage Angular 2 Elements with Events and Refs

--- src/app/simpe-form/simple-form.component.ts
+++ src/app/simpe-form/simple-form.component.ts
@@ -3,15 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-simple-form',
   template: `
-    <p>
-      simpe-form Works!
-    </p>
+    <div>
+
+      <!-- #myInput is a Reference to the input element -->
+      <input #myInput type='text'>
+    
+      <!-- Creates a (click) event and assigns a method created in the class -->
+      <button (click)="onClick(myInput.value)">Click Me!</button>
+    
+    </div>
   `,
   styles: []
 })
 export class SimpleFormComponent implements OnInit {
 
   constructor() { }
+
+  onClick(value) {
+    // console.log('Clicked!!!');
+    console.log(value);
+  }
 
   ngOnInit() {
   }
